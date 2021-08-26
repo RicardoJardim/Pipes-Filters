e989f38d983b28c179fc159a626a4e0c9471e7af
Ricardo <ricardo.l.jardim@hotmail.com>
2021-08-26 22:19:22 +0100

    Rest web service

---
+++ src/test/java/com/architecture/pipesandfilters/PipesandfiltersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.architecture.pipesandfilters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipesandfiltersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
